0f615a0ed26b7f7649d0cb6c5d4057f39e6d5485
Jiseok97 <wltjr1426@naver.com>
2021-07-19 20:46:33 +0900

    Add passive each job

--- lifeGame/ViewController.swift
+++ lifeGame/ViewController.swift
@@ -8,125 +8,122 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: View Color
+    @IBOutlet weak var headView: UIView!
     
     
     // MARK: Head Prop
     @IBOutlet weak var levleTextLbl: UILabel!
     @IBOutlet weak var moneyLbl: UILabel!
     @IBOutlet weak var characterImg: UIImageView!
-    var money : Int = 14990
+    var money : Int = 4995
     
     
-    // MARK: Skill View
+    
+    
+    // MARK: 패시브 스킬 UIView
     @IBOutlet weak var firstSkillView: UIView!
+    @IBOutlet weak var explainLbl: UILabel!
+    @IBOutlet weak var passiveLbl: UILabel!
     
     
-    // MARK: View Color
-    @IBOutlet weak var headView: UIView!
+    // MARK: 액티브 스킬1 UIView
+    @IBOutlet weak var secondSkillView: UIView!
+    @IBOutlet weak var activeSkillBtn: UIButton!
+    @IBOutlet weak var activeExplainLbl: UILabel!
     
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 그라데이션
-//        headView.setGradient(color1: UIColor.darkGray, color2: UIColor.lightGray)
-//        setZ()
-        passiveSkill()
-        setImage()
-    }
-    
- 
-    // MARK: Set ImageView & Level Label
-    func setImage()
-    {
-        DispatchQueue.global(qos: .userInteractive).async {
-            DispatchQueue.main.async {
-                if (self.money <= 5000) {
-                    self.levleTextLbl.text = "흙수저"
-                    self.characterImg.image = UIImage(named: "level1")
-                }
-                else if (self.money > 5000 && self.money <= 10000) {
-                    self.levleTextLbl.text = "동수저"
-                    self.characterImg.image = UIImage(named: "level2")
-                }
-                else if (self.money > 10000 && self.money <= 15000) {
-                    self.levleTextLbl.text = "은수저"
-                    self.characterImg.image = UIImage(named: "level3")
-                }
-                else {
-                    self.levleTextLbl.text = "금수저"
-                    self.characterImg.image = UIImage(named: "level4")
-                }
-            }
-        }
+        gameStart()
     }
     
     
-    // MARK: 패시브 스킬
-    func passiveSkill() {
-        DispatchQueue.global(qos: .background).async {
+    
+    
+    // MARK: 게임 전체적 스레드
+    func gameStart() {
+        DispatchQueue.global(qos: .userInitiated).async {
             while(true) {
-                self.money += 1
+                self.passiveSkill()
                 usleep(1000000)
                 DispatchQueue.main.async {
                     self.moneyLbl.text = String(self.money) + " 원"
+                    self.setImage()
+                    self.activeSkillBtn.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
                 }
             }
         }
     }
     
     
-    // MARK: zPositon Function
-    func setZ()
+ 
+    // MARK: 이미지 및 라벨 변경 기능
+    func setImage()
     {
-        headView.layer.zPosition = 0
-        levleTextLbl.layer.zPosition = 10
-        moneyLbl.layer.zPosition = 10
+        if (self.money <= 5000) {
+            self.levleTextLbl.text = "흙수저"
+            self.characterImg.image = UIImage(named: "level1")
+        }
+        else if (self.money > 5000 && self.money <= 10000) {
+            self.levleTextLbl.text = "동수저"
+            self.characterImg.image = UIImage(named: "level2")
+        }
+        else if (self.money > 10000 && self.money <= 15000) {
+            self.levleTextLbl.text = "은수저"
+            self.characterImg.image = UIImage(named: "level3")
+        }
+        else if (self.money > 15000){
+            self.levleTextLbl.text = "금수저"
+            self.characterImg.image = UIImage(named: "level4")
+        }
     }
-}
-
-
-
-
-
-
-
-
-
-// MARK: View 그라데이션 기능
-extension UIView{
-    func setGradient(color1:UIColor,color2:UIColor){
-        let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.colors = [color1.cgColor,color2.cgColor]
-        gradient.locations = [0.0 , 1.0]
-        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
-        gradient.frame = bounds
-        layer.addSublayer(gradient)
+    
+    
+    
+    // MARK: LEVEL마다 패시브 차별화 기능
+    func passiveSkill()
+    {
+        DispatchQueue.main.async {
+            if (self.levleTextLbl.text == "흙수저") {
+                self.money += 1
+                self.explainLbl.text = "패 시 브\n돈 줍기"
+                self.firstSkillView.backgroundColor = .brown
+                self.passiveLbl.text = "💵 + 1$ / s"
+            }
+            else if (self.levleTextLbl.text == "동수저") {
+                self.money += 10
+                self.explainLbl.text = "패 시 브\n통장 이자"
+                self.explainLbl.textColor = .brown
+                self.levleTextLbl.textColor = .brown
+                self.firstSkillView.backgroundColor = .systemGreen
+                self.passiveLbl.text = "💵 + 10$ / s"
+            }
+            else if (self.levleTextLbl.text == "은수저") {
+                self.money += 150
+                self.explainLbl.text = "패 시 브\n일 보너스"
+                self.explainLbl.textColor = .lightGray
+                self.levleTextLbl.textColor = .lightGray
+                self.firstSkillView.backgroundColor = .systemIndigo
+                self.passiveLbl.text = "💵 + 150$ / s"
+            }
+            else if (self.levleTextLbl.text == "금수저") {
+                self.money += 1000
+                self.explainLbl.text = "패 시 브\n난 일 안 해"
+                self.explainLbl.textColor = .systemYellow
+                self.firstSkillView.backgroundColor = .systemRed
+                self.levleTextLbl.textColor = .systemYellow
+                self.passiveLbl.text = "💵 + 1000$ / s"
+            }
+        }
     }
-}
-
-
-
-// MARK: Button 그라데이션 + 테두리
-// 사용 예시
-// testView.gradientButton("Hello", startColor: .red, endColor: .blue)
-extension UIView{
-
-    func gradientButton(_ buttonText:String, startColor:UIColor, endColor:UIColor) {
-
-        let button:UIButton = UIButton(frame: self.bounds)
-        button.setTitle(buttonText, for: .normal)
-
-        let gradient = CAGradientLayer()
-        gradient.colors = [startColor.cgColor, endColor.cgColor]
-        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
-        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
-        gradient.frame = self.bounds
-        self.layer.insertSublayer(gradient, at: 0)
-        self.mask = button
-
-        button.layer.cornerRadius =  button.frame.size.height / 2
-        button.layer.borderWidth = 5.0
+  
+    @objc func buttonTapped() {
+        self.activeSkillBtn.isEnabled = false
+        
+        self.money += 50
+        self.activeSkillBtn.isEnabled = true
     }
 }

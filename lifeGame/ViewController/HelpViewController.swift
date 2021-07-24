8fb05131e8b06f6affe627029a1d0f0f9f1b6de8
Jiseok97 <wltjr1426@naver.com>
2021-07-24 13:37:04 +0900

    Change help images

--- lifeGame/ViewController/HelpViewController.swift
+++ lifeGame/ViewController/HelpViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-var imagesLst = [ "0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png" ]
+var imagesLst = [ "0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png" ]
 
 class HelpViewController: UIViewController {
     
